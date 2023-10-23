363f7b8c49484593e1080c8306d902df4ee2d1f4
SunilHO <133845155+SunilHO@users.noreply.github.com>
2023-10-23 23:16:20 +0530

    Update AccountRelatedObjects.js

--- AccountRelatedObjects.js
+++ AccountRelatedObjects.js
@@ -4,10 +4,10 @@ import getRelatedObjects from '@salesforce/apex/AccountRelatedObjects.getRelated
 
 export default class AccountRelatedObjects extends LightningElement {
     @api recordId;
-    AccountRecord;
-    AccountColumns = [];
-    ContactRecord; 
-    ContactColumns = [];
+    accountRecord;
+    accountColumns = [];
+    contactRecord; 
+    contactColumns = [];
 
   //getting all the related object records
     @wire(getRelatedObjects,{
@@ -20,19 +20,19 @@ export default class AccountRelatedObjects extends LightningElement {
          var childAccountList =  relatedObjects.childAccountList;
          var contactList =  relatedObjects.contactList;
 
-         for(let Rec in ChildAccountList[0]){
+         for(let Rec in childAccountList[0]){
             accCol.push( { label: Rec, fieldName: Rec});
         }
        //Assigning the columns & data for Child AccountList
-       this.AccountColumns = accCol;
-       this.AccountRecord = childAccountList;
+       this.accountColumns = accCol;
+       this.accountRecord = childAccountList;
 
-         for(let Rec in ContactList[0]){
+         for(let Rec in contactList[0]){
                 contCol.push( { label: Rec, fieldName: Rec });
             }
           //Assigning the columns & data for Contacts.
-           this.ContactColumns = contCol;
-           this.ContactRecord = contactList;
+           this.contactColumns = contCol;
+           this.contactRecord = contactList;
 
         }
         else if(error){
